3060b6cde7ed55f7498412ded5cf99bae2591bb8
Michael Sylvester <msylvester@cnm.edu>
2026-01-29 15:18:52 -0700

    Starting playing cards project

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+java {
+   toolchain {
+       languageVersion.set(JavaLanguageVersion.of(21))
+   }
+}
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/edu/cnm/deepdive/cards/model/Suit.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.cards.model;
+
+public enum Suit {
+
+  CLUBS,
+  DIAMONDS,
+  HEARTS,
+  SPADES;
+
+  public enum Color {
+    BLACK, RED
+  }
+
+}
