598aabe158beb4c9098fe879f6d53e98d7a202c3
Michael Sylvester <msylvester@cnm.edu>
2026-02-05 11:08:47 -0700

    Updated Java version to 25, added card symbols and ranks to resource bundle, and refactored `View` for localized card formatting.

--- src/main/java/edu/cnm/deepdive/cards/Main.java
+++ src/main/java/edu/cnm/deepdive/cards/Main.java
@@ -1,9 +1,5 @@
 package edu.cnm.deepdive.cards;
 
-import edu.cnm.deepdive.cards.model.Deck;
-import edu.cnm.deepdive.cards.service.Trick;
-import java.util.random.RandomGenerator;
-
 public class Main {
 
   public static void main(String[] args) {
--- src/main/java/edu/cnm/deepdive/cards/View.java
+++ src/main/java/edu/cnm/deepdive/cards/View.java
@@ -7,7 +7,9 @@ import edu.cnm.deepdive.cards.service.Trick;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 import java.util.random.RandomGenerator;
+import java.util.stream.Collectors;
 
 public class View {
 
@@ -18,6 +20,14 @@ public class View {
       Comparator.comparing(Card::getColor)
           .reversed()
           .thenComparing(Comparator.naturalOrder());
+
+  private static final String BUNDLE_BASE_NAME = "card";
+  private static final String COMPOSITION_FORMAT_KEY = "composition-format";
+  private static final ResourceBundle bundle;
+
+  static {
+    bundle = ResourceBundle.getBundle("card");
+  }
 
   void perform () {
     System.out.println("Are you ready for a card trick?");
@@ -30,14 +40,27 @@ public class View {
     System.out.println("Now we will swap the cards.");
     int numSwaps = trick.swap();
     System.out.println("We swapped " + numSwaps + " cards.");
-    Map<Color, List<Card>> result = trick.getResult();
-    TrickResult representation = new TrickResult(result.get(Color.BLACK), result.get(Color.RED));
+    TrickResult representation = new TrickResult(trick.getResult());
     System.out.println("Here is the result of the trick.");
     System.out.println(representation);
 
   }
 
   private record TrickResult(List<Card> blackPile, List<Card> redPile) {
+
+    public TrickResult(Map<Color, List<Card>> piles) {
+      List<Card> sortedBlackPile = piles
+          .get(Color.BLACK)
+          .stream()
+          .sorted(BLACK_FIRST_COMPARATOR)
+          .toList();
+      List<Card> sortedRedPile = piles
+          .get(Color.RED)
+          .stream()
+          .sorted(RED_FIRST_COMPARATOR)
+          .toList();
+      this(sortedBlackPile, sortedRedPile);
+    }
 
     @Override
     public String toString() {
@@ -49,8 +72,28 @@ public class View {
           .stream()
           .filter(card -> card.getColor() == Color.BLACK)
           .count();
-      return "Red Pile (" + redInRedCount + "): " + redPile +
-          "\nBlack Pile (" + blackInBlackCount + "): " + blackPile;
+      String compositionFormat = bundle.getString(COMPOSITION_FORMAT_KEY);
+      String blackPileRepr = blackPile
+          .stream()
+          .map((card) -> {
+            return compositionFormat.formatted(
+                bundle.getString(card.getRank().name()),
+                bundle.getString(card.getSuit().name())
+            );
+          })
+          .collect(Collectors.joining(", "));
+      String redPileRepr = redPile
+          .stream()
+          .map((card) -> {
+            return compositionFormat.formatted(
+                bundle.getString(card.getRank().name()),
+                bundle.getString(card.getSuit().name())
+            );
+          })
+          .collect(Collectors.joining(", "));
+
+      return "Red Pile (" + redInRedCount + "): " + redPileRepr +
+          "\nBlack Pile (" + blackInBlackCount + "): " + blackPileRepr;
     }
   }
 }
